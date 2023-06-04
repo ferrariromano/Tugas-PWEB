854e3f2eacb094fc966fbbdccf29df79089f7652
ferrariromano <ferrariromano74@gmail.com>
2023-06-05 01:47:12 +0700

    fix fitur dokter

--- app/Http/Controllers/DokterController.php
+++ app/Http/Controllers/DokterController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class DaftarDokterController extends Controller
+class DokterController extends Controller
 {
     public function index()
     {
